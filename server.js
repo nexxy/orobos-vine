d5d3f25758fb64a5ad194a138ba780fe5ac37660
Emily Rose <emily@contactvibe.com>
2012-08-07 04:40:20 -0700

    Added inbound control system for arming and disarming the alarm system. Refactor of outbound code.

--- server.js
+++ server.js
@@ -1,6 +1,35 @@
 ;(function() {
 
-    var fs = require('fs');
+    /**
+    * REQUIRES
+    */
+    var 
+        express = require('express') 
+        , twilio = require('twilio-api')
+        , async = require('async')
+        , dnode = require('dnode')
+        , path = require('path')
+        , net = require('net')
+        , fs = require('fs')
+    ;
+
+    /**
+    * UTILITY STUFFS
+    */
+    var log = function() {
+
+        var 
+            d = (new Date()).toLocaleString()
+            , format = arguments[0]
+        ;
+
+        delete arguments[0];
+        console.log(
+
+            ["[", d, "] ", format].join('')
+            , arguments
+        );
+    };
 
     /**
     * Load and verify configuration
@@ -9,8 +38,7 @@
 
         try {
 
-            var buff = fs.readFileSync('./config.json');
-
+            var buff = fs.readFileSync(path.resolve(__dirname, 'config.json'));
             var 
                 conf = JSON.parse(buff)
                 , error = undefined
@@ -20,11 +48,6 @@
 
                 error = "Invalid accountSid";
             }
-            else if((!conf.numbers) || !conf.numbers.from 
-                || !conf.numbers.to) {
-
-                error = "Invalid number parameters";
-            }
             else if(!conf.authToken) {
 
                 error = "Invalid authToken";
@@ -33,144 +56,324 @@
 
                 error = "Invalid accountSid";
             }
-            else if(!(conf.ports) || !conf.ports.app 
-                || !conf.ports.device) {
+            else if((!conf.number) || !conf.number.from 
+                || !conf.number.to) {
 
-                error = "Invalid port specified";
+                error = "Invalid number parameters";
             }
-            else if (!(conf.codes) || !conf.codes.disarm.value 
-                || !conf.codes.disarm.phonetic || !conf.codes.connect.value
-                || !conf.codes.connect.phonetic) {
+            else if(!(conf.port) || !conf.port.app 
+                || !conf.port.device) {
+
+                error = "Invalid ports specified";
+            }
+            else if (!(conf.code) || (!conf.code.deactivate) || (!conf.code.activate)
+                || (!conf.code.connect) || !conf.code.connect.phonetic || !conf.code.connect.value
+                || !conf.code.deactivate.value || !conf.code.deactivate.phonetic 
+                || !conf.code.connect.value || !conf.code.connect.phonetic) {
 
                 error = "Invalid codes specified";
             }
+            else if((!conf.prompt) || !conf.prompt.passcode
+                || !conf.prompt.trigger || !conf.prompt.conference
+                || !conf.prompt.activate || !conf.prompt.deactivate) {
 
+                error = "Invalid prompts specified";
+            }
+            else if((!conf.response) || !conf.response.activated
+                || !conf.response.deactivated || !conf.response.unrecognized
+                || !conf.response.invalid) {
+
+                error = "Invalid responses specified.";
+            }
+            else if ((!conf.status) || !conf.status.phrase 
+                || !conf.status.active || !conf.status.inactive) {
+
+                error = "Invalid status specified."
+            }
             if(error) {
 
-                console.log("config.json: %s\r\n", error);
+                log("config.json: %s\r\n", error);
                 process.exit(1);
             }
         }
         catch (e) {
 
-            console.log("config.json: %s\r\n", e);
+            log("config.json: %s\r\n", e);
             process.exit(1);
         }
 
         return conf;
     })();
 
+    /**
+    * BOOTSTRAP
+    */
     var
-        express = require('express') 
-        , twilio = require('twilio-api')
-        , async = require('async')
-        , dnode = require('dnode')
-        , net = require('net')
-        , app = express.createServer()
+        app = express.createServer()
         , online = {}           // device controllers online
         , active = {}           // device controllers triggered
+        , currentCalls = 0      // calls in progress
         , voiceActive = false   // in a call
+        , alarmActive = true   // system is active (will make calls on alarms)
+        , alarmCheck = undefined
         , twapp                 // twilio app
         , cli = new twilio.Client(
 
             config.accountSid
             , config.authToken
         )
+        , fem = { "voice" : "woman" }
         , server = net.createServer(function(c) {
 
-            var d = dnode({
-
-                heartbeat : heartbeat
-                , detection : detection
-                , baseline : baseline
-            });
+            var d = dnode(DeviceInterface);
 
             c.pipe(d).pipe(c);
         })
     ;
 
     app.use(cli.middleware());
-    app.listen(config.ports.app);
-    server.listen(config.ports.device);
+    app.listen(config.port.app);
+    server.listen(config.port.device);
 
-    cli.account.getApplication(config.applicationSid, function(err, twip) {
+    var DeviceInterface = {
 
-    	if(err) { 
+        heartbeat : function heartbeat(id, cb) {
 
-    		console.log(err);
-    		return;
-    	}
+            if(!id || typeof cb !== "function") { 
+
+                log("Invalid ID or callback in heartbeat");
+                cb("invalid id/callback");
+                return; 
+            }
+
+            cb(null, id);
+        }
+        , detection : function detection(id, stat, cb) {
+
+            log("Received detection from device: %s, type: ", id, stat ? "lost USB" : "triggered");
+            
+            if(!activeTime()) {
+
+                cb(null, id);
+                return;
+            }
+            
+            if(!id || typeof cb !== "function") {
+
+                log("Invalid ID or callback in detection");
+                return;
+            }
+
+            active[id] = true;
+
+            if((id) && online[id] && !voiceActive && (alarmActive)) {
+
+                voiceActive = true;
+                callEveryone(id, cb);
+
+                return;
+            }
+            else if(voiceActive) {
+
+                cb(null, id);
+            }
+            else if(!alarmActive) {
+
+                log("Received detection from device %s, system is not active.", id);
+            }
+            else{
+
+                log("DEBUG id: %s active: %s voiceActive: %s", id, online[id], voiceActive);
+            }
+
+            cb(null, id);
+        }
+        , register : function register(id, cb) {
+
+            if(typeof cb !== "function") { return; }
+
+            this.registered = (new Date()).valueOf();
+
+            if(id) {
+
+                online[id] = true;
+                cb(null, id);
+            }
+            else {
+
+                log("Device attempted to register with invalid device ID.");
+                cb(new Error("Invalid device ID"), null);
+                return;
+            }
+        }
+    };
+
+    var app = function app(err, twip) {
+
+        if(err) { 
+
+            log(err);
+            process.exit(1);
+            return;
+        }
 
         twapp = twip;
-    	twip.register();
 
-    });
+        var callPasscode = function callPasscode(call, cb) {
+
+            return call.gather(cb, { 
+
+                numDigits : codes.passcode.length
+                , timeout : config.responseTimeout 
+            }).say(config.prompt.passcode, fem);
+        };
+
+        var callDeactivate = function callDeactivate(call, cb) { 
+
+            var code = config.code.deactivate.value;
+
+            return call.gather(cb, { 
+
+                numDigits : code.length
+                , timeout : config.responseTimeout 
+            }).say(config.prompt.deactivate, fem);
+        };
+
+        var callActivate = function callActivate(call, cb) {
+
+            var 
+                codeVal = config.code.activate.value
+                , codePhon = conf.code.activate.phonetic
+                , spoken = config.prompt.activate.replace('%s', codePhon)
+            ;
+
+            return call.gather(cb, { 
+
+                numDigits : codeVal.length
+                , timeout : config.responseTimeout 
+            }).say(spoken, fem);
+        };
+
+        var verifyDeactivate = function verifyDeactivate(call, digits) {
+
+            if(digits == config.code.deactivate.value) {
+
+                call.say(config.response.deactivated, fem);
+                call.hangup();
+                deactivate();
+            }
+            else {
+
+                call.say(config.response.unrecognized, fem);
+                callDeactivate(call, verifyDeactivate);
+            }
+        };
+
+        var verifyActivate = function verifyActivate(call, digits) {
+
+            if(digits == config.code.activate.value) {
+
+                call.say(config.response.activated, fem);
+                call.hangup();
+                activate();
+            }
+            else {
+                
+                call.say(config.response.unrecognized, fem);
+                callActivate(call, verifyActivate);
+            }
+        };
+
+        var verifyPasscode = function verifyPasscode(call, digits) {
+
+            if(digits != config.code.passcode) {
+
+                call.say(config.response.invalid, fem);
+                callPasscode(call, verifyPasscode);
+                return;
+            }
+
+            var 
+                systemStatus = config.status[
+                    alarmActive ? "active" : "inactive"
+                ]
+                , spoken = config.status.phrase.replace('%s', systemStatus)
+            ;
+
+            call.say(config.response.authed, fem);
+            call.say(spoken, fem);
+
+            if(alarmActive) {
+
+                callDeactivate(call, verifyDeactivate);
+            }
+            else {
+
+                callActivate(call, verifyActivate);
+            }
+        };
+
+        var incomingCall = function incomingCall(call) {
+
+            callPasscode(call, verifyPasscode);
+        };
+
+        twip.on('incomingCall', incomingCall);
+        twip.register();
+    };
+
+    cli.account.getApplication(config.applicationSid, app);
 
     /**
-    * Received baseline measurement
+    * Activated schedule hack 
+    * TODO: make this configurable.
     */
-    var baseline = function baseline(id, val, cb) {
+    var activeTime = function activeTime() {
 
-        if(!id || typeof cb !== "function") {
+        return true;
+        var hour = (new Date()).getHours();
 
-            console.log("Invalid ID or callback in baseline");
-            return;
+        if(hour >= 3 && hour <= 11) {
+
+            if(!alarmActive) {
+
+                log("Alarm is now active.");
+            }
+
+            alarmActive = true;
+            return true;
         }
+        if(alarmActive) {
 
-        console.log("Received baseline from device: %s", id);
-        online[id] = true;
-        cb(null, id);
+            log("Alarm is now inactive.");
+        }
+        alarmActive = false;
+        return false;
     };
 
     /**
-    * Received heartbeat (status::0)
+    * Deactivate the alarm and reset the watch timer
     */
-    var heartbeat = function recordHeartbeat(id, cb) {
+    var deactivate = function deactivate() {
 
-        if(!id || typeof cb !== "function") { 
+        alarmActive = false;
+        alarmWatch();
+    };
 
-            console.log("Invalid ID or callback in heartbeat");
-            return; 
-        }
+    var activate = function activate() {
 
-        cb(null, id);
+        alarmActive = true;
+        alarmWatch();
     };
 
     /**
-    * Received detection (status::1)
+    * Restart the watch timer (one hour)
     */
-    var detection = function recordDetection(id, cb) {
+    var alarmWatch = function alarmWatch() {
 
-        console.log("Received detection from device: %s", id);
-
-        if(!id || typeof cb !== "function") {
-
-            console.log("Invalid ID or callback in detection");
-            return;
-        }
-
-        active[id] = true;
-
-        if((id) && online[id] && !voiceActive) {
-
-            voiceActive = true;
-            console.log("active: Alerting...");
-
-            callEveryone(id, cb);
-
-            return;
-        }
-        else if(voiceActive) {
-
-            console.log("active: Already on call...");
-        }
-        else{
-
-            console.log("DEBUG id: %s active: %s voiceActive: %s", id, online[id], voiceActive);
-        }
-
-        cb(null, id);
-    };
+        clearInterval(alarmCheck);
+        alarmCheck = setInterval(activeTime, 60*60*1000);
+    }
 
     /**
     * Alert everyone on the call list
@@ -178,8 +381,7 @@
     var callEveryone = function callEveryone(id, done) {
         
         var calls = [];
-
-        config.numbers.to.forEach(function(num) {
+        config.number.to.forEach(function(num) {
 
             calls.push(getCaller(id, num));
         });
@@ -189,11 +391,11 @@
             if(err) {
 
                 done(err, undefined);
-                console.log(err);
+                log(err);
                 return;
             }
 
-            console.log(res);
+            log(res);
             done(null, res);
         });
     };
@@ -216,32 +418,29 @@
 
         if(!num || !id || typeof cb !== "function") {
 
-            console.log("Invalid voiceAlert parameters.");
+            log("Invalid voiceAlert parameters.");
             return false;
         }
 
-        console.log("voiceAlert %s %s", id, num);
+        log("Calling %s due to trigger from %s", num, id);
 
         /**
         * Get input from the user
         */
         var getDigits = function getDigits(call, cb) {
 
+            var 
+                digits = config.code.deactivate.value.length
+                , spoken = config.prompt.deactivate
+                    .replace(config.code.deactivate.phonetic)
+            ;
+
+            log("Prompting %s to enter selection...");
             return call.gather(cb, {
 
-                timeout : 10
-                , numDigits : 3
-            }).say([
-                "Press"
-                , config.codes.disarm.phonetic
-                , "to deactivate. Press"
-                , config.codes.connect.phonetic
-                , "to connect."
-                ].join(" ")
-            , {
-
-                voice : 'woman'
-            });
+                timeout : config.responseTimeout
+                , numDigits : digits
+            }).say(spoken, fem);
         };
 
         /**
@@ -249,20 +448,26 @@
         */
         var verifyDigits = function verifyDigits(call, digits) {
 
-            console.log("Digits entered: %s", digits);
-            if(digits == config.codes.disarm.value) {
+            log("%s entered digits: %s", num, digits);
 
-                active[id] = false;
+            /**
+            * User has disarmed the alarm
+            */
+            if(digits == config.code.deactivate.value) {
 
-                endCall(call, "The alarm has been deactivated.");
+                log("%s has deactivated the alarm.", num);
+                deactivate();
+                endCall(call, config.response.deactivated);
             }
-            else if(digits == config.codes.connect.value) {
+            else if(digits == config.code.connect.value) {
 
-                joinConference(call);
+                log("%s is joining the conference...", num);
+                joinConference(call);  
             }
             else {
 
-                say(call, "I did not recognize your selection.");
+                log("%s entered an unrecognized selection.", num);
+                say(call, config.response.unrecognized);
                 getDigits(call, verifyDigits);
             }            
         };
@@ -273,8 +478,8 @@
         var endCall = function endCall(call, message) {
 
             call.say(
-                (message ? message : '') + ' Goodbye.'
-                , { voice : 'woman' }
+                (message ? message : '') + config.response.hangup
+                , fem
             );
             call.hangup();
         };
@@ -301,7 +506,7 @@
 
             if(!call) { return; }
 
-            say(call, 'Connecting... Press star to disconnect.');
+            say(call, config.response.connecting);
             call.joinConference('orobos', {
 
                 waitUrl : ''
@@ -310,43 +515,47 @@
         };
 
         /**
-        * The conference has ended
+        * Called when the conference has ended
         */
         var endConference = function endConference(call, status) {
 
-            console.log("Conference ended...");
-            console.log(status);
+            log("Conference ended with status: %s.", status);
         };
 
-        twapp.makeCall(config.numbers.from, num, function(err, call) {
+        twapp.makeCall(config.number.from, num, function(err, call) {
 
             if(err) throw err;
 
+            currentCalls++;
             call.on('connected', function(status) {
 
                 //Called when the caller picks up
-                call.say("The alarm has been triggered!", {
-
-                    voice : 'woman'
-                    , loop : 3
-                });
-
                 getDigits(call, verifyDigits);
-
-                console.log("Speaking...");
+                log("Call with %s connected. Notifying user of alarm from %s.", num, id);
             });
 
             call.on('ended', function(status, duration) {
 
-                //Called when the call ends
-                voiceActive = false;
+                log("Call with %s has ended after %s seconds (%s).", num, duration, status);
 
                 /**
-                * Alarm was not deactivated
+                * No more calls active
                 */
-                if(active[id]) {
+                if(--currentCalls <= 0) {
+
+                    voiceActive = false;
+                    currentCalls = 0;
+                    log("All calls have ended.");
+                }
+
+                /**
+                * Alarm was not deactivated, call again.
+                * TODO: add a maximum retry limit.
+                */
+                if(alarmActive) {
 
                     voiceAlert(id, num, cb);
+                    log("Alarm still active, recalling %s.", num);
                     return;
                 }
 
@@ -354,5 +563,7 @@
             });
         });    	
     };
+
+    alarmWatch();
 
 })();
